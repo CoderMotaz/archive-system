32b853b32bf372e18a7c0502521a2f13022268cc
CoderMotaz <codermotaz@gmail.com>
2025-12-23 12:38:51 +0300

    Update database.php

---
+++ archive-system/config/database.php
@@ -0,0 +1,11 @@
+<?php
+// config/database.php
+return [
+    'host' => 'localhost',
+    'database' => 'enterprise_archive',
+    'username' => 'root', // غيّر حسب إعداداتك
+    'password' => '', // غيّر حسب إعداداتك
+    'charset' => 'utf8',
+    'collation' => 'utf8_bin',
+    'port' => 3306
+];
